dd3530a6a7e2258d7381b2bd857d079d7d332004
ljy0932 <ljy0932@nate.com>
2022-10-18 17:44:01 +0900

    카메라 접근 페이지 ui 변경

--- app/src/main/java/com/example/hairsee/menu/CamSub/Camera3Fragment.java
+++ app/src/main/java/com/example/hairsee/menu/CamSub/Camera3Fragment.java
@@ -1,19 +1,32 @@
 package com.example.hairsee.menu.CamSub;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.hairsee.Hairlist;
 import com.example.hairsee.R;
+import com.example.hairsee.detection.DetectorActivity;
 import com.example.hairsee.listClass.Hair;
+import com.example.hairsee.utils.MyAlert;
+import com.example.hairsee.utils.OnSingleClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,10 +37,24 @@ import java.util.List;
  */
 public class Camera3Fragment extends Fragment {
     public View view;
+    public ImageView camerstart_iv;
+    private Activity mainActivity;
     public RecyclerView hairChoice,hairChoice2; //컬,스트레이트
     private ArrayList<Hair> items=new ArrayList<>();
     private ArrayList<Hair> items2=new ArrayList<>();
     private Hairlist mHairlist;
+    private Context context;
+    public static final int Toast_Result = 1500;
+    public static final int PERMISSIONS_REQUEST = 1;
+    private static final int REQ = 0;
+    public static int permissionCamera;
+    public static int permissionRead;
+    public static int permissionWrite;
+    public static final String[] CAM_PERMISSIONS = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +68,9 @@ public class Camera3Fragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_camera3, container, false);
         hairChoice = view.findViewById(R.id.hairChoice1);
         hairChoice2 = view.findViewById(R.id.hairChoice2);
+        camerstart_iv = view.findViewById(R.id.camerstart_iv);
+        context = getContext();
+        mainActivity = getActivity();
         curlhair();
         straighthair();
         mHairlist = new Hairlist(items);
@@ -48,7 +78,51 @@ public class Camera3Fragment extends Fragment {
         hairChoice.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
         hairChoice2.setAdapter(mHairlist);
         hairChoice2.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        camerstart_iv.setOnClickListener(new OnSingleClickListener() {
+            @Override
+            public void onSingleClick(View v) {
+                permission();
+                permissionCamera = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA);
+                permissionRead = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE);
+                permissionWrite = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                if (permissionCamera != PackageManager.PERMISSION_GRANTED
+                        || permissionRead != PackageManager.PERMISSION_GRANTED
+                        || permissionWrite != PackageManager.PERMISSION_GRANTED) {
+                    MyAlert.MyDialog_single(context, "안내", "카메라 및 저장공간 권한을 허용해주십시오", v1 -> {
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + context.getPackageName()));
+                        context.getApplicationContext().startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                    });
+                }
+                else {
+                    btnRegistrationClickHandler();
+                }
+            }
+        });
         return view;
+    }
+    private void permission(){
+        // 카메라, 오디오, 저장공간 확인
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        ) {
+            btnRegistrationClickHandler();
+
+        } else {
+            requestVideoPermissions(); // 권한 확인
+
+        }
+    }
+    public void requestVideoPermissions() {
+        ActivityCompat.requestPermissions(mainActivity,CAM_PERMISSIONS, PERMISSIONS_REQUEST);
+    }
+
+    //btnRegistration
+    private void btnRegistrationClickHandler() {
+        Intent intent = new Intent(context, DetectorActivity.class);
+        intent.putExtra("btnFlag", "registraion");
+        startActivityForResult(intent, Toast_Result);
     }
     public void curlhair() {
 
