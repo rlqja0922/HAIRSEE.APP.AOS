eb1a01225842941f3cdee2f6015c06b308ca2026
rlqja0922 <ljy0932@nate.com>
2022-12-02 15:14:11 +0900

    프로그레스바 추가

--- app/src/main/java/com/example/hairsee/detection/WaitActivity.java
+++ app/src/main/java/com/example/hairsee/detection/WaitActivity.java
@@ -3,9 +3,12 @@ package com.example.hairsee.detection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.hairsee.API.HairRequest;
@@ -13,6 +16,7 @@ import com.example.hairsee.API.RetrofitInterface;
 import com.example.hairsee.MainActivity;
 import com.example.hairsee.R;
 import com.example.hairsee.ResultActivity;
+import com.example.hairsee.utils.MyFirebaseMessagingService;
 import com.example.hairsee.utils.SharedStore;
 
 import java.io.File;
@@ -38,12 +42,17 @@ public class WaitActivity extends AppCompatActivity implements MainActivity.OnBa
     private Context mContext;
     private Timer timers;
     private String waitStep = "0";
+    private ProgressBar wait_pb;
+    private TextView wait_pt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wait);
         mContext = getApplicationContext();
         ImgAPI();
+        wait_pb = findViewById(R.id.wait_pb);
+        wait_pt = findViewById(R.id.wait_pt);
         ImageView img_gif = findViewById(R.id.gif);
         Glide.with(this).load(R.drawable.hairlod).into(img_gif);
 
@@ -53,6 +62,18 @@ public class WaitActivity extends AppCompatActivity implements MainActivity.OnBa
                 @Override
                 public void run() {
                     waitStep = SharedStore.getWait(mContext);
+                    if (waitStep=="스텝2"){
+                        wait_pb.setProgress(2);
+                        wait_pt.setText("2/3");
+                    }else if (waitStep=="스텝3"){
+                        wait_pb.setProgress(3);
+                        wait_pt.setText("3/3");
+                        Intent intent;
+                        intent = new Intent(WaitActivity.this,ResultActivity.class);
+                        intent.putExtra("url",SharedStore.getimgURL(mContext));
+                        Log.d("fcm", "imgurl: "+SharedStore.getimgURL(mContext));
+                        startActivity(intent);
+                    }
                 }
             };
             timers.scheduleAtFixedRate(timerTask,0,5000);
